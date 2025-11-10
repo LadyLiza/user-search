e27aceebb4f6c25b75e7e9c07fa5e76cc618682f
LadyLiza <elizabet132010@gmail.com>
2025-11-10 04:07:42 +0100

    fix

--- src/API/index.ts
+++ src/API/index.ts
@@ -2,5 +2,5 @@ import { User } from "@/types/user";
 import { get } from "@/utils/http";
 
 export const fetchUsers = async (): Promise<User[]> => {
-  return await get<User[]>("/users123");
+  return await get<User[]>("/users");
 };
